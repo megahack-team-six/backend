2ea7306f840e3508eaa13b16a968085a007bc5d4
Gabriel <gabrielfjggyn@gmail.com>
2020-02-17 08:13:10 -0300

    Resolved on issue #1

--- api/usuarios/create.php
+++ api/usuarios/create.php
@@ -1,6 +1,6 @@
 <?php
 include_once '../config/header_post.php';
-$rules = array('usuario.criar');
+// $rules = array('usuario.criar');
 include_once '../config/validate.php';
 include_once '../config/database.php';
 include_once '../objects/users.php';
@@ -13,18 +13,13 @@ $data = json_decode(file_get_contents("php://input"));
 if (
     isset($data->nome) && !empty($data->nome) &&    
     isset($data->email) && !empty($data->email) &&    
-    isset($data->password) && !empty($data->password) &&    
-    isset($data->repetPassword) && !empty($data->repetPassword)
+    isset($data->password) && !empty($data->password)
 ) {
     $user->nome = $data->nome;
     $user->email = $data->email;
     $user->password = $data->password;
-    $user->repetPassword = $data->repetPassword;
 
-    if ($data->password != $data->repetPassword) {
-        http_response_code(400);
-        echo json_encode(array("message" => "As senhas não conferem."));
-    } else if ($id = $user->create()) {
+    if ($id = $user->create()) {
         http_response_code(200);
         echo json_encode(array("message" => "Usuário criado.", "id" => $id));
     } else {
